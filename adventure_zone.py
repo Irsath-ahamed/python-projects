6156413777ea9ce5c63cdd8e1d26e5b1f1839f19
Irsath Ahamed <irzathahamed52@gmail.com>
2023-07-29 20:25:10 +0530

    updated on adventure

--- adventure_zone.py
+++ adventure_zone.py
@@ -12,7 +12,26 @@ if answer == 'left':
     elif answer == 'walk':
         print('you are walked many miles and died. you lost..!')
     else:
-        print('pls enter valid answer')
+        print('pls enter valid answer, you lost')
+
+elif answer == "right":
+    answer = input(
+        "You come to a bridge, it looks wobbly, do you want to cross it or head back (cross/back)? ")
+
+    if answer == "back":
+        print("You go back and lose.")
+    elif answer == "cross":
+        answer = input(
+            "You cross the bridge and meet a stranger. Do you talk to them (yes/no)? ")
+
+        if answer == "yes":
+            print("You talk to the stanger and they give you gold. You WIN!")
+        elif answer == "no":
+            print("You ignore the stranger and they are offended and you lose.")
+        else:
+            print('Not a valid option. You lose.')
+    else:
+        print('Not a valid option. You lose.')
 
 else:
     print('Not a valid option. You lose.')
